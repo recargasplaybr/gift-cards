7c1d332c2998488d8cc2c8f71d3ce792f9b107c3
recargasplaybr <recargasplaybr@gmail.com>
2026-08-04 10:59:19 -1200

    Update script.js

--- players/script.js
+++ players/script.js
@@ -335,7 +335,7 @@ function buy(){
     let numero = "5532999561915"; 
     
     // Teste: Das 6:00 ás 12:00 usa o número novo
-    if (horaAtual >= 16 && horaAtual < 12) {
+    if (horaAtual >= 6 && horaAtual < 12) {
         numero = "5532998427529";
     }
     
